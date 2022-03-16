cb7519374c43adface947d6a708cedbd42d0461d
KingKickass <mbauer@thetrask.com>
2022-03-16 10:27:24 +0100

    created common wrapper - Card

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,17 +1,18 @@
 import Date from '../Date/Date';
+import Card from '../Card/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
     console.log(props.data);
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <Date date={props.data.date}/>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.amount} £</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
--- src/components/ExpensesContainer/ExpensesContainer.js
+++ src/components/ExpensesContainer/ExpensesContainer.js
@@ -1,15 +1,16 @@
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import Card from "../Card/Card";
 import './ExpensesContainer.css';
 
 function ExpensesContainer(props) {
 
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem data={props.expenses[0]}/>
             <ExpenseItem data={props.expenses[1]}/>
             <ExpenseItem data={props.expenses[2]}/>
             <ExpenseItem data={props.expenses[3]}/>
-        </div>
+        </Card>
     )
 }
 
