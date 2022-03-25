3809ba34f7539b1141fab318a2effdc8b332c047
KingKickass <mbauer@thetrask.com>
2022-03-25 17:59:27 +0100

    no message

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import ExpenseItem from './components/ExpenseItem/ExpenseItem';
 import ExpensesContainer from './components/ExpensesContainer/ExpensesContainer';
+import NewExpense from './components/NewExpense/NewExpense';
 import './App.css';
 
 function App() {
   const expenses = [
-    {title: 'Rent', amount: 4500, date: new Date(2021,5,12)},
-    {title: 'Booze', amount: 6000, date: new Date(2022,2,22)},
-    {title: 'Hoes', amount: 99999, date: new Date(2022,1,1)},
-    {title: 'Food', amount: 16.99, date: new Date(2022,3,15)}
+    { title: 'Rent', amount: 4500, date: new Date(2021, 5, 12) },
+    { title: 'Booze', amount: 6000, date: new Date(2022, 2, 22) },
+    { title: 'Hoes', amount: 99999, date: new Date(2022, 1, 1) },
+    { title: 'Food', amount: 16.99, date: new Date(2022, 3, 15) }
   ];
+
+  const onSubmitHandler = (ed) => {
+    const newExpense = {
+      ...ed
+    }
+  }
+
   return (
     <div>
       <div>
         <h1>Title</h1>
+        <div>
+          <NewExpense onSubmitNewExpense={onSubmitHandler} />
+        </div>
       </div>
-        <ExpensesContainer expenses={expenses}/>
+      <ExpensesContainer expenses={expenses} />
     </div>
   );
 }
--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,17 +1,25 @@
+import React, { useState } from 'react';
 import Date from '../Date/Date';
 import Card from '../Card/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    console.log(props.data);
+    //console.log(props.data);
+
+    const [title, setTitle] = useState(props.data.title);
+
+    function handleClick() {
+        setTitle(`${title} Updated`);
+    }
 
     return (
         <Card className='expense-item'>
             <Date date={props.data.date}/>
             <div className='expense-item__description'>
-                <h2>{props.data.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>{props.data.amount} £</div>
             </div>
+            <button onClick={handleClick}>Change title</button>
         </Card>
     )
 }
